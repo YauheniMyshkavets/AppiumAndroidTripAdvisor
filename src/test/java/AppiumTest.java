4712ba2494804d81b2a1cf196983bfc473fa2f1e
Evgeny <evgenymyshkovets@gmail.com>
2019-11-11 16:22:58 +0300

    added several actions

--- src/test/java/AppiumTest.java
+++ src/test/java/AppiumTest.java
@@ -1,11 +1,12 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,37 +16,48 @@ import java.net.URL;
 public class AppiumTest {
     public AppiumDriver<MobileElement> driver;
     public WebDriverWait wait;
-    DesiredCapabilities caps = new DesiredCapabilities();
-
 
     @BeforeMethod
     public void setup () throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Pixel 3 XL API 29");
+        caps.setCapability("deviceName", "Pixel 3 XL");
         caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "9");
         caps.setCapability("skipUnlock","true");
         caps.setCapability("appPackage", "com.tripadvisor.tripadvisor");
-        caps.setCapability("appActivity","com.tripadvisor.tripadvisor.TripAdvisoTripAdvisorActivity");
+        caps.setCapability("appActivity","com.tripadvisor.tripadvisor.TripAdvisorTripAdvisorActivity");
         caps.setCapability("noReset","false");
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver<MobileElement>(url,caps);
         wait = new WebDriverWait(driver, 10);
     }
 
+//    @AfterMethod
+//    public void finish () {
+//        driver.quit();
+//    }
+
+
     @Test
     public void firstMobileTestTest () {
 
-        MobileElement mainPage = driver.findElement(By.id("com.tripadvisor.tripadvisor:id/splash_screen"));
-        mainPage.click();
+        //tab to get started button
+        wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("com.tripadvisor.tripadvisor:id/generic_onboarding_cta")))).click();
 
+        //tap to close button
+        wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("com.tripadvisor.tripadvisor:id/login_skip")))).click();
 
+        //tap to not now button
+        wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("com.tripadvisor.tripadvisor:id/no_thanks")))).click();
+
+        (new TouchAction(driver))
+                .press(PointOption.point(1371, 1315))
+                .moveTo(PointOption.point(110, 1321))
+                .release()
+                .perform();
 
     }
 
-    @AfterMethod
-    public void finish () {
-        driver.quit();
-    }
+
 }
